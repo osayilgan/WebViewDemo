f8f02640abe2fef5fee44a934e528ca515de6ec3
Okan SAYILGAN <osayilgan@gmail.com>
2016-01-01 21:26:51 -0800

    Changed : the name of the external breezy directory from "breezy" to "mWebViewDemo" Added : Another method to create an app specific directory for dex maker library's cache files.

--- WebViewDemo/app/src/main/java/apps/okan/demo/webview/util/FileUtils.java
+++ WebViewDemo/app/src/main/java/apps/okan/demo/webview/util/FileUtils.java
@@ -1,5 +1,6 @@
 package apps.okan.demo.webview.util;
 
+import android.content.Context;
 import android.os.Environment;
 
 import java.io.File;
@@ -9,6 +10,7 @@ import java.io.File;
  */
 public class FileUtils {
 
+
     /**
      * Creates or returns the "breezy" Folder in external storage directory.
      *
@@ -16,12 +18,16 @@ public class FileUtils {
      */
     public static File getExternalBreezyDirectory() {
 
-        File directory = new File(Environment.getExternalStorageDirectory(), "/breezy");
+        File directory = new File(Environment.getExternalStorageDirectory(), "/mWebViewDemo");
 
         if (directory.exists() == false) {
-            directory.mkdir();
+            directory.mkdirs();
         }
 
         return directory;
+    }
+
+    public static File getDexCacheDirectory(Context context) {
+        return context.getDir("dex-cache", Context.MODE_PRIVATE);
     }
 }
